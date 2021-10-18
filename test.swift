d90896c19b86dcac9e9a9888392e45e3037b78a1
youngwooJang <duddn5906@naver.com>
2021-10-18 16:44:05 +0900

    STEP1: 간이 코드

--- test.swift
+++ test.swift
@@ -8,9 +8,10 @@ enum Juice {
     case 딸바
     case 바딸
 
-    func returnRecipe() -> [.Fruit: Int] {
+    //Dictionary vs Tuple 
+    func returnRecipe() -> [ .Fruit: Int ] {
         switch self {
-
+            return [ .딸기: 3, .바나나: 1 ]
         }
     }
 }
@@ -24,7 +25,7 @@ struct Fruit {
 }
 
 class FruitStore {
-    var fruits = [Fruit] 
+    var fruits = [Fruit: Int] 
     
     수량변경(n개) {
         += n
@@ -36,15 +37,24 @@ struct JuiceMaker {
     let fruitStore = FruitStore()
 
     제조() {
-        if 재고확인() == true {
+        if 재고확인(recipe) == true {
             과일쥬스생성(param: .Juice)
+        } else {
+            print제조불가()
         }
+    }
+
+    재고확인() -> Bool {
+        
     }
     
     과일쥬스생성(param: .Juice) {
         let recipe = param.getRecipe()
 
-        for fruit in recipe {
+        for (fruit, number) in recipe {
+
+            fruitStore.[fruit] -= number
+
             수량변경()
         }
 
